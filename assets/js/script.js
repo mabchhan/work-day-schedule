126aed3053ccf27b0a7266571e9cdc991cc5da46
Mab <mabchhan@gmail.com>
2022-11-16 11:10:51 -0800

    modified add element, and load data from localstorage to function

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,51 +15,58 @@ $(function () {
     "hour-17",
   ];
 
-  for (var i = 0; i < 9; i++) {
-    var mainContainer = $(".container-lg");
-    var container = $('<div class="row time-block "></div>');
-    container.attr("id", id[i]);
-    // container.text(id[i]);
+  function generateElement() {
+    for (var i = 0; i < 9; i++) {
+      var mainContainer = $(".container-lg");
+      var container = $('<div class="row time-block "></div>');
+      container.attr("id", id[i]);
+      // container.text(id[i]);
 
-    var textHour = $(
-      '<div class="col-2 col-md-1 hour text-center py-3"></div>'
-    );
-    var hour = id[i].split("-")[1];
+      var textHour = $(
+        '<div class="col-2 col-md-1 hour text-center py-3"></div>'
+      );
+      var hour = id[i].split("-")[1];
 
-    textHour.text(hour + "AM");
-    if (hour >= 12) {
-      textHour.text(hour - 12 + "PM");
+      textHour.text(hour + "AM");
+      if (hour === "12") {
+        textHour.text("12PM");
+      }
+      if (hour > 12) {
+        textHour.text(hour - 12 + "PM");
+      }
+
+      var textArea = $(
+        '<textarea class="col-8 col-md-10 description" rows="3"> </textarea>'
+      );
+
+      var button = $(
+        '<button class="btn saveBtn col-2 col-md-1" aria-label="save"></button>'
+      );
+
+      var img = $('<i class="fas fa-save" aria-hidden="true"></i>');
+
+      mainContainer.append(container);
+      container.append(textHour);
+      container.append(textArea);
+      container.append(button);
+      button.append(img);
     }
-
-    var textArea = $(
-      '<textarea class="col-8 col-md-10 description" rows="3"> </textarea>'
-    );
-
-    var button = $(
-      '<button class="btn saveBtn col-2 col-md-1" aria-label="save"></button>'
-    );
-
-    var img = $('<i class="fas fa-save" aria-hidden="true"></i>');
-
-    mainContainer.append(container);
-    container.append(textHour);
-    container.append(textArea);
-    container.append(button);
-    button.append(img);
   }
+  generateElement();
+
   // saveBtn click listener
   $(".saveBtn").on("click", function () {
     // Get nearby values of the description in JQuery
     var text = $(this).siblings(".description").val();
-    var time = $(this).parent().attr("id");
-
+    var time = $(this).siblings(".hour").text();
+    console.log(text, time);
     // Save text in local storage
     localStorage.setItem(time, text);
   });
 
-  function timeTracker() {
+  function setColorOnBlock() {
     //get current hours.
-    var currentHour = dayjs().format("HH");
+    var currentHour = dayjs().hour();
 
     // loop over time blocks
     $(".time-block").each(function () {
@@ -67,9 +74,9 @@ $(function () {
 
       // To check the time and add the classes for background indicators
       if (timeBlock < currentHour) {
+        $(this).addClass("past");
         $(this).removeClass("future");
         $(this).removeClass("present");
-        $(this).addClass("past");
       } else if (timeBlock === currentHour) {
         $(this).removeClass("past");
         $(this).removeClass("future");
@@ -79,22 +86,37 @@ $(function () {
         $(this).removeClass("past");
         $(this).addClass("future");
       }
-      // console.log(timeBlock);
     });
   }
-  timeTracker();
+  setColorOnBlock();
 
   // get value from local storage
 
-  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
-  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
-  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
-  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
-  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
-  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
-  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
-  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
-  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+  function getValueFromStorage() {
+    $(".hour").each(function () {
+      var keyIndex = $(this).text(); // key in localstorage
+      var valueIndex = localStorage.getItem(keyIndex); // value in localstorage pair with keyIndex
+
+      if (valueIndex !== null) {
+        $(this).siblings(".description").val(valueIndex);
+      }
+    });
+  }
+
+  getValueFromStorage();
+  // $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+  // $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  // $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  // $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  // $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+  // $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+  // $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+  // $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+  // $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+
+  // for (var j = 0; j < 9; j++) {
+  //   $('"#" + id[j] ".description"').val(localStorage.getItem(id[j]));
+  // }
 
   //
   // display the current date in the header of the page.
